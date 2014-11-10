643da247452ec1c3c217f931dd27b15ec81267ea
Derek Elkins <derek.a.elkins@gmail.com>
2014-11-10 09:26:26 -0500

    Some fugly but functional directory listing code.

--- Main.hs
+++ Main.hs
@@ -1,23 +1,27 @@
 module Main where
+import Control.Applicative ( liftA2 ) -- base
+import Control.Monad ( when ) -- base
 import qualified Data.ByteString as BS -- bytestring
+import qualified Data.ByteString.Char8 as CBS -- bytestring
 import qualified Data.ByteString.Lazy as LBS -- bytestring
-import Data.Maybe ( fromMaybe, maybe )
-import Data.Monoid ( (<>) )
-import Data.String ( fromString ) -- for AuthRealm
-import System.Console.GetOpt ( getOpt, usageInfo, OptDescr(..), ArgDescr(..), ArgOrder(..) )
-import System.Environment ( getArgs )
-import System.IO ( putStrLn )
-import Network.HTTP.Types.Status ( status403, status404 ) -- http-types
-import Network.Wai ( Application, responseLBS ) -- wai
-import Network.Wai.Middleware.Static ( staticPolicy, addBase, isNotAbsolute, noDots, Policy ) -- wai-middleware-static
+import Data.List ( sort ) -- base
+import Data.Maybe ( fromMaybe, maybe ) -- base
+import Data.Monoid ( (<>) ) -- base
+import Data.String ( fromString ) -- base
+import System.Console.GetOpt ( getOpt, usageInfo, OptDescr(..), ArgDescr(..), ArgOrder(..) ) -- base
+import System.Directory ( getDirectoryContents, doesDirectoryExist, getModificationTime  ) -- directory
+import System.Environment ( getArgs ) -- base
+import System.FilePath ( (</>) ) -- filepath
+import System.IO ( putStrLn ) -- base
+import Network.HTTP.Types.Status ( status200, status403, status404 ) -- http-types
+import Network.Wai ( Application, Middleware, rawPathInfo, responseLBS ) -- wai
+import qualified Network.Wai.Handler.Warp as Warp -- warp
+import qualified Network.Wai.Handler.WarpTLS as Warp -- warp-tls
 import Network.Wai.Middleware.Gzip ( gzip, gzipFiles, def, GzipFiles(GzipIgnore, GzipCompress) ) -- wai-extra
 import Network.Wai.Middleware.HttpAuth ( basicAuth, AuthSettings ) -- wai-extra
 import Network.Wai.Middleware.Local ( local ) -- wai-extra
 import Network.Wai.Middleware.RequestLogger ( logStdout ) -- wai-extra
-import qualified Network.Wai.Handler.Warp as Warp -- warp
-import qualified Network.Wai.Handler.WarpTLS as Warp -- warp-tls
-
--- Directory listing?
+import Network.Wai.Middleware.Static ( staticPolicy, addBase, isNotAbsolute, noDots, Policy ) -- wai-middleware-static
 
 -- Maybe future things: virtual hosts, caching, PUT/POST/DELETE support, dropping permissions, client certificates
 -- Not future things: CGI etc of any sort
@@ -75,6 +79,8 @@ options = [
         "Enable compression. (Default)",
     Option "" ["no-compress"] (NoArg (\opt -> opt { optCompress = False })) 
         "Disable compression.",
+    Option "" ["no-listings"] (NoArg (\opt -> opt { optDirectoryListings = False })) 
+        "Don't list directory contents.",
     Option "r" ["realm"] (OptArg (\r opt -> opt { optRealm = fromMaybe "" r }) "REALM") 
         "Set the authentication realm. It's recommended to use this with HTTPS. (Default: \"\")",
     Option "" ["password"] (OptArg (\pw opt -> opt { optPassword = fromString $ fromMaybe "" pw }) "PASSWORD") 
@@ -110,8 +116,26 @@ enableIf :: Bool -> (a -> a) -> a -> a
 enableIf True f = f
 enableIf    _ _ = id
 
+-- TODO: Make this less fugly.
+directoryListing :: Options -> FilePath -> Middleware -- TODO: Handle exceptions.  Note, this isn't critical.  It will carry on.
+directoryListing opts baseDir app req k = do
+    let path = baseDir </> CBS.unpack (BS.tail $ rawPathInfo req) -- TODO: This unpack is ugly.
+    b <- doesDirectoryExist path
+    if not b then app req k else do
+        when (optVerbose opts) $ putStrLn $ "Rendering listing for " ++ path
+        html <- fmap container (mapM (\p -> fileDetails p (path </> p)) =<< fmap sort (getDirectoryContents path))
+        k (responseLBS status200 [] html)
+  where fileDetails label f = liftA2 (renderFile label f) (doesDirectoryExist f) (getModificationTime f)
+        renderFile label path isDirectory modTime = LBS.concat $ map fromString [
+            "<tr><td>", if isDirectory then "d" else "f", "</td><td><a href=\"/", path, "\">", label, "</a></td><td>", show modTime, "</td></tr>"
+          ]
+        container xs
+          = fromString "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"><head><title>sws</title><style type=\"text/css\">a, a:active {text-decoration: none; color: blue;}a:visited {color: #48468F;}a:hover, a:focus {text-decoration: underline; color: red;}body {background-color: #F5F5F5;}h2 {margin-bottom: 12px;}table {margin-left: 12px;}th, td { font: 90% monospace; text-align: left;}th { font-weight: bold; padding-right: 14px; padding-bottom: 3px;}td {padding-right: 14px;}td.s, th.s {text-align: right;}div.list { background-color: white; border-top: 1px solid #646464; border-bottom: 1px solid #646464; padding-top: 10px; padding-bottom: 14px;}div.foot { font: 90% monospace; color: #787878; padding-top: 4px;}</style></head><body><div class=\"list\"><table><tr><td></td><td>Name</td><td>Last Modified</td></tr>"
+         <> LBS.concat xs
+         <> fromString "</table></div><div class=\"foot\">sws 0.1.0.0</div></body></html>"
+
 app404 :: Application
-app404 req k = k (responseLBS status404 [] LBS.empty)
+app404 req k = k (responseLBS status404 [] (fromString "File Not Found"))
 
 serve :: Options -> String -> IO ()
 serve (Options { optHelp = True }) _ = putStrLn $ usageInfo usageHeader options
@@ -128,13 +152,14 @@ serve opts dir =
                                (fromString $ optRealm opts))
                 $ enableIf (optCompress opts) (gzip def { gzipFiles = GzipCompress })
                 $ staticPolicy (basePolicy <> addBase dir) 
-                -- $ directoryListing -- TODO
+                $ enableIf (optDirectoryListings opts) (directoryListing opts dir)
                 $ app404
   where runner | optHTTPS opts = Warp.runTLS tlsSettings (Warp.setPort (optPort opts) Warp.defaultSettings)
                | otherwise = Warp.run (optPort opts)
             where tlsSettings = (Warp.tlsSettings (optCertificate opts) (optKeyFile opts)) { 
                     Warp.onInsecure = if optAllowHTTP opts then Warp.AllowInsecure else Warp.DenyInsecure (fromString "Use HTTPS") } 
 
+main :: IO ()
 main = do
     args <- getArgs
     case getOpt Permute options args of
