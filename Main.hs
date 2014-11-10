ce93a4524263f1df8d5fdf46c4da21a1341fd90f
Derek Elkins <derek.a.elkins@gmail.com>
2014-11-10 04:54:48 -0500

    Basically working.  No HTTPS.  No directory listing.  No options.

--- Main.hs
+++ Main.hs
@@ -1,13 +1,20 @@
 module Main where
+import qualified Data.ByteString.Lazy as LBS
 import Data.Maybe ( maybe )
-import System.Console.GetOpt
+import Data.Monoid ( (<>) )
+import System.Console.GetOpt ( getOpt, usageInfo, OptDescr(..), ArgDescr(..), ArgOrder(..) )
 import System.Environment ( getArgs )
 import System.IO ( putStrLn )
+import Network.HTTP.Types.Status ( status404 )
+import Network.Wai ( Application, responseLBS )
+import Network.Wai.Middleware.Static ( staticPolicy, addBase, isNotAbsolute, noDots, Policy )
+import Network.Wai.Middleware.Gzip ( gzip, gzipFiles, def, GzipFiles(GzipCompress) )
+import qualified Network.Wai.Handler.Warp as Warp
+-- import qualified Network.Wai.Handler.WarpTLS as WarpTLS
 
 -- wai-extra for GZip compression
 -- wai-tls for HTTPS
 -- wai-middleware-static maybe for serving the files themselves
--- System.Console.GetOpt for commandline arguments
 -- Directory listing?
 
 data Options = Options {
@@ -26,8 +33,17 @@ options = [
 usageHeader :: String
 usageHeader = "Usage: sws [OPTIONS...] [DIRECTORY]"
 
+basePolicy :: Policy
+basePolicy = noDots <> isNotAbsolute
+
+-- TODO: Add directory listing.
+app404 :: Application
+app404 req k = k (responseLBS status404 [] LBS.empty)
+
 serve :: Options -> String -> IO ()
-serve opts dir = print opts
+serve opts dir = do
+    putStrLn $ "Listening on port " ++ show (optPort opts)
+    Warp.run (optPort opts) $ gzip def { gzipFiles = GzipCompress } $ staticPolicy (addBase dir <> basePolicy) $ app404
 
 main = do
     args <- getArgs
